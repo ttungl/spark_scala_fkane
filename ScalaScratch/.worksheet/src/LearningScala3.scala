77ac10470b1f9a1025b93ef303b8cf5b2d2d6837
ttungl <ttungl@gmail.com>
2020-03-10 21:33:06 -0700

    added Section 2: Scala crash course

---
+++ ScalaScratch/.worksheet/src/LearningScala3.scala
@@ -0,0 +1,58 @@
+object LearningScala3 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(225); 
+  // Functions
+  
+  // Format is def <function name>(parameter name: type...) : return type = { expression }
+  // Don't forget the = before the expression!
+  def squareIt(x: Int) : Int = {
+  	x * x
+  };System.out.println("""squareIt: (x: Int)Int""");$skip(43); 
+  
+  def cubeIt(x: Int): Int = {x * x * x};System.out.println("""cubeIt: (x: Int)Int""");$skip(26); 
+  
+  println(squareIt(2));$skip(24); 
+  
+  println(cubeIt(2));$skip(124); 
+  
+  // Functions can take other functions as parameters
+  
+  def transformInt(x: Int, f: Int => Int) : Int = {
+  	f(x)
+  };System.out.println("""transformInt: (x: Int, f: Int => Int)Int""");$skip(44); 
+  
+  val result = transformInt(2, squareIt);System.out.println("""result  : Int = """ + $show(result ));$skip(19); 
+  println (result);$skip(209); val res$0 = 
+  
+  // "Lambda functions", "anonymous functions", "function literals"
+  // You can declare functions inline without even giving them a name
+  // This happens a lot in Spark.
+  transformInt(3, x => x * x * x);System.out.println("""res0: Int = """ + $show(res$0));$skip(34); val res$1 = 
+  
+  transformInt(10, x => x / 2);System.out.println("""res1: Int = """ + $show(res$1));$skip(50); val res$2 = 
+  
+  transformInt(2, x => {val y = x * 2; y * y});System.out.println("""res2: Int = """ + $show(res$2));$skip(424); 
+  
+  // This is really important!
+  
+  // EXERCISE
+  // Strings have a built-in .toUpperCase method. For example, "foo".toUpperCase gives you back FOO.
+  // Write a function that converts a string to upper-case, and use that function of a few test strings.
+  // Then, do the same thing using a function literal instead of a separate, named function.
+  
+  def convertUpperString(s : String) : String = {
+  	s.toUpperCase
+  };System.out.println("""convertUpperString: (s: String)String""");$skip(21); 
+  
+  val s = "hello";System.out.println("""s  : String = """ + $show(s ));$skip(33); 
+  println(convertUpperString(s));$skip(81); 
+  
+  def transformString(s: String, f:String => String) : String = {
+  	f(s)
+  };System.out.println("""transformString: (s: String, f: String => String)String""");$skip(54); val res$3 = 
+  
+  
+  transformString("hihihi", convertUpperString);System.out.println("""res3: String = """ + $show(res$3));$skip(48); val res$4 = 
+  transformString("hahaha", convertUpperString);System.out.println("""res4: String = """ + $show(res$4));$skip(48); val res$5 = 
+  transformString("hohoho", convertUpperString);System.out.println("""res5: String = """ + $show(res$5));$skip(53); val res$6 = 
+  transformString("hello world", convertUpperString);System.out.println("""res6: String = """ + $show(res$6))}
+  
+}
---
+++ ScalaScratch/LearningScala3.sc
@@ -0,0 +1,59 @@
+object LearningScala3 {
+  // Functions
+  
+  // Format is def <function name>(parameter name: type...) : return type = { expression }
+  // Don't forget the = before the expression!
+  def squareIt(x: Int) : Int = {
+  	x * x
+  }                                               //> squareIt: (x: Int)Int
+  
+  def cubeIt(x: Int): Int = {x * x * x}           //> cubeIt: (x: Int)Int
+  
+  println(squareIt(2))                            //> 4
+  
+  println(cubeIt(2))                              //> 8
+  
+  // Functions can take other functions as parameters
+  
+  def transformInt(x: Int, f: Int => Int) : Int = {
+  	f(x)
+  }                                               //> transformInt: (x: Int, f: Int => Int)Int
+  
+  val result = transformInt(2, squareIt)          //> result  : Int = 4
+  println (result)                                //> 4
+  
+  // "Lambda functions", "anonymous functions", "function literals"
+  // You can declare functions inline without even giving them a name
+  // This happens a lot in Spark.
+  transformInt(3, x => x * x * x)                 //> res0: Int = 27
+  
+  transformInt(10, x => x / 2)                    //> res1: Int = 5
+  
+  transformInt(2, x => {val y = x * 2; y * y})    //> res2: Int = 16
+  
+  // This is really important!
+  
+  // EXERCISE
+  // Strings have a built-in .toUpperCase method. For example, "foo".toUpperCase gives you back FOO.
+  // Write a function that converts a string to upper-case, and use that function of a few test strings.
+  // Then, do the same thing using a function literal instead of a separate, named function.
+  
+  def convertUpperString(s : String) : String = {
+  	s.toUpperCase
+  }                                               //> convertUpperString: (s: String)String
+  
+  val s = "hello"                                 //> s  : String = hello
+  println(convertUpperString(s))                  //> HELLO
+  
+  def transformString(s: String, f:String => String) : String = {
+  	f(s)
+  }                                               //> transformString: (s: String, f: String => String)String
+  
+  
+  transformString("hihihi", convertUpperString)   //> res3: String = HIHIHI
+  transformString("hahaha", convertUpperString)   //> res4: String = HAHAHA
+  transformString("hohoho", convertUpperString)   //> res5: String = HOHOHO
+  transformString("hello world", convertUpperString)
+                                                  //> res6: String = HELLO WORLD
+  
+}
